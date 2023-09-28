52af7ae9a8b77960be87ab446ec944f7550307cb
ulisesaale <ulisesaale@gmail.com>
2023-09-28 16:35:01 -0300

    Se agrega proyecto

--- Desarrollo/2_Software/README.md
+++ Desarrollo/2_Software/README.md
@@ -1,1 +1,17 @@
 ![logos](/Desarrollo/assets/ISPC_portada.png)
+
+### **Se realiza la codificacion en Assembler para el funcionamiento de una Estacion Metereologica donde nos muestra Temperatura, Humedad y Presión atmosférica.**
+
+### Para este proyecto se utiliza el PIC18F4550.
+
+### Ventajas:
+
+* **Capacidad de procesamiento:** El PIC18F4550 tiene un núcleo PIC de 8 bits,  tiene una capacidad de procesamiento más avanzada y mayor cantidad de memoria Flash y RAM disponible. Esto permite una mayor flexibilidad en la programación y el manejo de datos, lo que puede ser beneficioso cuando se trabaja con múltiples sensores y dispositivos de visualización.
+
+* **Compatibilidad USB:** El PIC18F4550 es conocido por su capacidad USB incorporada, lo que significa que puedes utilizarlo para conectar el sistema a una computadora o dispositivo USB. Esto podría ser útil si deseas realizar una comunicación bidireccional con un ordenador para la configuración, la recopilación de datos o la visualización en tiempo real.
+
+* **Periféricos adicionales:** El PIC18F4550 también tiene una variedad de periféricos adicionales, como convertidores analógico-digitales (ADC) de 10 bits y UART, que pueden ser útiles en aplicaciones que requieren una mayor funcionalidad y flexibilidad en la adquisición de datos y la comunicación.
+
+* **Código más eficiente:** Dado que el PIC18F4550 tiene más recursos de memoria y una arquitectura de instrucción más avanzada, podrías ser capaz de escribir un código más eficiente y optimizado, lo que podría ser especialmente útil cuando se trabaja con varios sensores y dispositivos.
+
+![pic](/Desarrollo/assets/PIC18F4550.jfif)
--- Desarrollo/2_Software/main.asm
+++ Desarrollo/2_Software/main.asm
@@ -1,173 +1,76 @@
-;====================================================================
-; Archivo Main.asm generado por el asistente de nuevo proyecto
-;
-; Creado:   mar. sep. 5 2023
-; Procesador: PIC18F4550
-; Compilador: MPASM (Proteus)
-;====================================================================
-;====================================================================
-; DEFINICIONES
-;====================================================================
-#include "p18f4550.inc" ; Incluye el archivo de definición de registros
-;====================================================================
-; VARIABLES
-;====================================================================
-; Definición de constantes
-LCD_RS  EQU 0x1B    ; Puerto para RS del LCD
-LCD_E   EQU 0x1A    ; Puerto para E del LCD
-LCD_D4  EQU 0x1D    ; Puerto para D4 del LCD
-LCD_D5  EQU 0x1C    ; Puerto para D5 del LCD
-LCD_D6  EQU 0x1F    ; Puerto para D6 del LCD
-LCD_D7  EQU 0x1E    ; Puerto para D7 del LCD
-BMP280_SDI EQU 0x33 ; Puerto para SDI del BMP280
-BMP280_SDO EQU 0x34 ; Puerto para SDO del BMP280
-BMP280_SCK EQU 0x35 ; Puerto para SCK del BMP280
-BMP280_CSB EQU 0x36 ; Puerto para CSB del BMP280
-DHT22_PIN  EQU 0x17  ; Puerto para el DHT22
-; Zona de Datos
-CBLOCK 0x20
-    Temp_BMP280         ; Variable para la temperatura del BMP280
-    Presion_BMP280     ; Variable para la presión del BMP280
-    Humedad_DHT22      ; Variable para la humedad del DHT22
-    Delay1             ; Variable para el retardo
-    Delay2             ; Variable para el retardo
-    Delay3             ; Variable para el retardo
-ENDC
-;====================================================================
-; PROGRAMA PRINCIPAL
-;====================================================================
-ORG 0x00
-GOTO Inicio
-;====================================================================
-; Rutinas para LCD LM016L, BMP280 y DHT22 (con datos simulados)
-;====================================================================
-; Rutina para inicializar el LCD LM016L
-Inicio:
-    ; Configurar pines del LCD como salida
-    BCF STATUS, RP0       ; Cambiar al banco 0 de registros
-    MOVLW 0x00            ; Puerto A como salida
-    MOVWF TRISA
-    MOVLW 0xF0            ; Puerto C como salida
-    MOVWF TRISC
-    BCF STATUS, RP0       ; Volver al banco 0 de registros
-    ; Inicializar el LCD
-    CALL EsperarMs         ; Esperar un tiempo para la inicialización
-    MOVLW 0x03             ; Modo de 4 bits
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    CALL EsperarMs         ; Esperar un tiempo para la inicialización
-    MOVLW 0x03             ; Modo de 4 bits
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    CALL EsperarMs         ; Esperar un tiempo para la inicialización
-    MOVLW 0x03             ; Modo de 4 bits
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    MOVLW 0x02             ; Cambiar a modo de 8 bits
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    MOVLW 0x28             ; Configurar el LCD en 2 líneas, 5x8 caracteres
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    MOVLW 0x0C             ; Mostrar cursor, no parpadear
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    MOVLW 0x06             ; Desplazar cursor a la derecha
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
-    MOVLW 0x01             ; Borrar pantalla
-    CALL EnviarInstruccion ; Enviar instrucción al LCD
+;*********************************************************************************
+;							**
+;** --------------------------------------------------------------------------- **
+;** DESCRIPCIÓN													**
+;** Pone digitos de 0-9 en display 7-segmentos de 4_digitos, en el digito 4	**
+;**   el MOD_7S_4D_L (2009-05-06) conecta al PORTD segmentos y al PORTE digitos **
+;*********************************************************************************
+
+    list        p=16f877a   		; directiva para definir procesador
+    #include    <p16f877a.inc>  	; definiciones de variables especificas del procesador
+    
+    __CONFIG _CP_OFF & _WDT_OFF & _BODEN_OFF & _PWRTE_ON & _HS_OSC & _WRT_OFF & _LVP_OFF & _CPD_OFF
+
+     cblock 0x20	;separa epacio para 20 registros, 
+				; define 3 registros para retardo
+delay1              ; retardo 1
+delay2              ; retardo 2
+delay3			; retardo 3
+contador			; contador de digito
+     endc
+
+#define	_kte_delay3	h'0F'	
+
+     org 0
+Start:
+	clrf		PORTB		; apaga PUERTO D segmentos
+	clrf		PORTC		; apaga PUERTO E digitos
+
+	bsf    	STATUS,RP0     ; selecciona Banco 1
+	clrf		TRISB		; configura PUERTO D, salidas	
+	clrf		TRISC		; configura PUERTO E, salidas	
+	bcf    	STATUS,RP0     ; selecciona Banco 0
+
+	bsf		PORTC,2		; prende Digito 4
+
+	clrf		contador		; borre contador
+Main:
+	movf		contador,W		; cargue contador a W 
+	call		Tabla_code_7_seg	; llame a Tabla codigo de 7 segmentos
+	movwf	PORTB			; pongalo en PUERTO D
+	
+	movlw	_kte_delay3	; carga valor a 
+	movwf	delay3		; Retardo 3 de ALTO nivel
+
 Loop:
-    ; Leer datos del BMP280 y DHT22
-    CALL LeerBMP280
-    CALL LeerDHT22
-    ; Mostrar datos en el LCD
-    CALL MostrarDatosLCD
-    GOTO Loop
-; Rutina para enviar una instrucción al LCD
-EnviarInstruccion:
-    ; Configurar RS = 0 (Instrucción)
-    BCF PORTA, LCD_RS
-    ; Enviar el dato al LCD
-    MOVWF PORTC
-    ; Habilitar el LCD (E = 1)
-    BSF PORTA, LCD_E
-    ; Esperar un corto tiempo (puedes ajustarlo según tus necesidades)
-    CALL EsperarUs
-    ; Deshabilitar el LCD (E = 0)
-    BCF PORTA, LCD_E
-    ; Esperar un corto tiempo (puedes ajustarlo según tus necesidades)
-    CALL EsperarUs
-    RETURN
-; Rutina para esperar un tiempo en microsegundos
-EsperarUs:
-    ; Inserta aquí tu código para generar un retraso de microsegundos
-    RETURN
-; Rutina para esperar un tiempo en milisegundos
-EsperarMs:
-    ; Inserta aquí tu código para generar un retraso de milisegundos
-    RETURN
-; Rutina para leer datos simulados del BMP280
-LeerBMP280:
-    ; Leer datos simulados del BMP280 y almacenarlos en Temp_BMP280 y Presion_BMP280
-    ; Reemplaza esto con tus datos simulados
-    MOVLW 25
-    MOVWF Temp_BMP280
-    MOVLW 1000
-    MOVWF Presion_BMP280
-    RETURN
-; Rutina para leer datos simulados del DHT22
-LeerDHT22:
-    ; Leer datos simulados del DHT22 y almacenar la humedad en Humedad_DHT22
-    ; Reemplaza esto con tus datos simulados
-    MOVLW 60
-    MOVWF Humedad_DHT22
-    RETURN
-; Rutina para mostrar datos en el LCD
-MostrarDatosLCD:
-    ; Mostrar datos en el LCD
-    ; Posicionar cursor en la primera línea
-    MOVLW 0x80
-    CALL EnviarInstruccion
-    ; Mostrar temperatura
-    MOVLW 'T'
-    CALL EnviarDato
-    MOVLW 'e'
-    CALL EnviarDato
-    MOVLW 'm'
-    CALL EnviarDato
-    MOVLW 'p'
-    CALL EnviarDato
-    MOVLW ':'
-    CALL EnviarDato
-    ; Convertir el valor de temperatura a texto y mostrarlo
-    CALL ConvertirTexto
-    ; Posicionar cursor en la segunda línea
-    MOVLW 0xC0
-    CALL EnviarInstruccion
-    ; Mostrar humedad
-    MOVLW 'H'
-    CALL EnviarDato
-    MOVLW 'u'
-    CALL EnviarDato
-    MOVLW 'm'
-    CALL EnviarDato
-    MOVLW ':'
-    CALL EnviarDato
-    ; Convertir el valor de humedad a texto y mostrarlo
-    CALL ConvertirTexto
-    RETURN
-; Rutina para enviar un dato al LCD
-EnviarDato:
-    ; Configurar RS = 1 (Dato)
-    BSF PORTA, LCD_RS
-    ; Enviar el dato al LCD
-    MOVWF PORTC
-    ; Habilitar el LCD (E = 1)
-    BSF PORTA, LCD_E
-    ; Esperar un corto tiempo (puedes ajustarlo según tus necesidades)
-    CALL EsperarUs
-    ; Deshabilitar el LCD (E = 0)
-    BCF PORTA, LCD_E
-    ; Esperar un corto tiempo (puedes ajustarlo según tus necesidades)
-    CALL EsperarUs
-    RETURN
-; Rutina para convertir un valor numérico a texto y mostrarlo en el LCD
-ConvertirTexto:
-    ; Inserta aquí tu código para convertir el valor numérico a texto
-    RETURN
-; Otras rutinas y subrutinas necesarias
-END
+	decfsz  	delay1,f       ; decremente Retardo de BAJO nivel  
+	goto    	Loop    		; hay 3 ciclos por loop * 256 loopss = 768 instrucciones
+	decfsz  	delay2,f       ; decremente Retardo de MEDIO nivel  
+	goto    	Loop    		; hay por loop (768 + 3) * 256 cilcos = 197.376 ciclos
+	decfsz  	delay3,f       ; decremente Retardo de ALTO nivel
+	goto    	Loop    		; hay por loop (197.376 + 3) * 31 cilcos = 6.118.749 ciclos        
+
+	incf		contador		; sigueinte numero
+	movlw	.10			; cargue 10
+	xorwf	contador,W	; compara contador con 10
+	btfsc	STATUS,Z		; comparación NO es igual a 10
+	clrf		contador		; contador a 0
+
+	goto    	Main       	; haga ciclo
+
+Tabla_code_7_seg 			; Tabla para display de 7 segmentos.
+	addwf 	PCL,F
+
+	retlw 3Fh 	; numero "0".
+	retlw 06h 	; numero "1".
+	retlw 5Bh 	; numero "2".
+	retlw 4Fh 	; numero "3".
+	retlw 66h 	; numero "4".
+	retlw 6Dh 	; numero "5".
+	retlw 7Dh 	; numero "6".
+	retlw 07h 	; numero "7".
+	retlw 7Fh 	; numero "8".
+	retlw 67h 	; numero "9".
+    
+	end
+
